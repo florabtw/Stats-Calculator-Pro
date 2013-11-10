12d98b769db03baf71c62d7b505b0516ca49ceba
Nick Pierson <nkcrpn@gmail.com>
2013-11-10 15:53:56 -0600

    Tests wake lock functionality on perm & comb screen

--- test/src/me/nickpierson/StatsCalculatorPro/pc/ProPCPresenterTest.java
+++ test/src/me/nickpierson/StatsCalculatorPro/pc/ProPCPresenterTest.java
@@ -1,20 +1,32 @@
 package me.nickpierson.StatsCalculatorPro.pc;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import me.nickpierson.StatsCalculator.pc.PCPresenterTest;
+import me.nickpierson.StatsCalculatorPro.R;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowPreferenceManager;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 @Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner.class)
 public class ProPCPresenterTest extends PCPresenterTest {
 
 	Activity activity;
+	ProPCView proView;
+
+	private static final String WAKE_LOCK = "wake lock";
 
 	@Override
 	@Before
@@ -22,11 +34,36 @@ public class ProPCPresenterTest extends PCPresenterTest {
 		super.setup();
 
 		activity = mock(Activity.class);
+		proView = mock(ProPCView.class);
 	}
 
-	@Override
 	public void createPresenter() {
-		ProPCPresenter.create(activity, model, view);
+		ProPCPresenter.create(activity, model, proView);
 	}
 
+	@Test
+	public void whenPresenterIsCreated_ViewIsToldToKeepScreenOnIfPreferenceIsSet() {
+		Context context = Robolectric.application.getApplicationContext();
+		when(activity.getApplicationContext()).thenReturn(context);
+		when(activity.getString(R.string.wake_lock)).thenReturn(WAKE_LOCK);
+		SharedPreferences sharedPref = ShadowPreferenceManager.getDefaultSharedPreferences(context);
+		sharedPref.edit().putBoolean(WAKE_LOCK, true).commit();
+
+		createPresenter();
+
+		verify(proView).wakeLock();
+	}
+
+	@Test
+	public void whenPresenterIsCreated_ViewIsNeverToldToKeepScreenOnIfPreferenceIsNotSet() {
+		Context context = Robolectric.application.getApplicationContext();
+		when(activity.getApplicationContext()).thenReturn(context);
+		when(activity.getString(R.string.wake_lock)).thenReturn(WAKE_LOCK);
+		SharedPreferences sharedPref = ShadowPreferenceManager.getDefaultSharedPreferences(context);
+		sharedPref.edit().putBoolean(WAKE_LOCK, false).commit();
+
+		createPresenter();
+
+		verify(proView, never()).wakeLock();
+	}
 }
