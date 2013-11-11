2eb2ef8d566b114b0b07f00e418d651f6b1bb1f8
Nick Pierson <nkcrpn@gmail.com>
2013-11-11 17:41:52 -0600

    Constants file name change in library

--- src/me/nickpierson/StatsCalculatorPro/basic/ProBasicModel.java
+++ src/me/nickpierson/StatsCalculatorPro/basic/ProBasicModel.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import me.nickpierson.StatsCalculator.basic.BasicModel;
-import me.nickpierson.StatsCalculator.utils.MyConstants;
+import me.nickpierson.StatsCalculator.utils.Constants;
 import me.nickpierson.StatsCalculatorPro.utils.ProConstants;
 import android.app.Activity;
 
@@ -28,9 +28,9 @@ public class ProBasicModel extends BasicModel {
 	public HashMap<String, Double> calculateResults(List<Double> numberList) {
 		HashMap<String, Double> results = super.calculateResults(numberList);
 
-		double size = results.get(MyConstants.SIZE);
-		double sampleDev = results.get(MyConstants.SAMPLE_DEV);
-		double mean = results.get(MyConstants.ARITH_MEAN);
+		double size = results.get(Constants.SIZE);
+		double sampleDev = results.get(Constants.SAMPLE_DEV);
+		double mean = results.get(Constants.ARITH_MEAN);
 
 		results.put(ProConstants.STD_ERROR, sampleDev / Math.sqrt(size));
 		results.put(ProConstants.SUM_SQRS, calculateSumOfSquares(numberList, mean));
