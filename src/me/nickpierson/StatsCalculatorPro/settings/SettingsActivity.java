34173d296e32d462d4872340c7a05b3e633be7b1
Nick Pierson <nkcrpn@gmail.com>
2014-02-11 17:23:14 -0600

    Themes settings screen

--- src/me/nickpierson/StatsCalculatorPro/settings/SettingsActivity.java
+++ src/me/nickpierson/StatsCalculatorPro/settings/SettingsActivity.java
@@ -1,11 +1,10 @@
 package me.nickpierson.StatsCalculatorPro.settings;
 
 import me.nickpierson.StatsCalculatorPro.R;
-import me.nickpierson.StatsCalculatorPro.utils.ProThemeHelper;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.widget.ListView;
 
 public class SettingsActivity extends PreferenceActivity {
@@ -13,14 +12,20 @@ public class SettingsActivity extends PreferenceActivity {
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		ProThemeHelper.handleTheme(this);
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		boolean isDarkTheme = sharedPref.getBoolean(getString(R.string.dark_mode), false);
+		if (isDarkTheme) {
+			setTheme(R.style.DarkNotHomeTheme);
+		}
 
 		super.onCreate(savedInstanceState);
 
 		addPreferencesFromResource(R.xml.preferences);
 
-		// ListView list = getListView();
-		// list.setDivider(new ColorDrawable(Color.RED));
-		// list.setDividerHeight(10);
+		if (isDarkTheme) {
+			ListView list = getListView();
+			list.setDivider(getResources().getDrawable(R.drawable.divider_drawable));
+			list.setDividerHeight((int) (getResources().getDisplayMetrics().density));
+		}
 	}
 }
