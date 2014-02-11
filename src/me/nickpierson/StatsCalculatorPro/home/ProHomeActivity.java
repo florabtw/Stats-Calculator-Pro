61ffe16d034c6546e24819c89a99141c5e1a758e
Nick Pierson <nkcrpn@gmail.com>
2014-02-10 18:36:40 -0600

    Theme updates automatically after leaving settings screen

--- src/me/nickpierson/StatsCalculatorPro/home/ProHomeActivity.java
+++ src/me/nickpierson/StatsCalculatorPro/home/ProHomeActivity.java
@@ -4,16 +4,18 @@ import me.nickpierson.StatsCalculator.home.HomeActivity;
 import me.nickpierson.StatsCalculator.home.HomeModel;
 import me.nickpierson.StatsCalculatorPro.R;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ProHomeActivity extends HomeActivity {
+public class ProHomeActivity extends HomeActivity implements OnSharedPreferenceChangeListener {
+
+	private boolean toRestart = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
 		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
 		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
@@ -21,11 +23,26 @@ public class ProHomeActivity extends HomeActivity {
 			setTheme(R.style.DarkHomeTheme);
 		}
 
+		super.onCreate(savedInstanceState);
+
+		sharedPref.registerOnSharedPreferenceChangeListener(this);
+
 		view = new ProHomeView(this);
 		model = new HomeModel();
 		ProHomePresenter.create(this, model, view);
 
 		setContentView(view.getView());
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		if (toRestart) {
+			toRestart = false;
+			finish();
+			startActivity(getIntent());
+		}
 	}
 
 	@Override
@@ -42,6 +59,13 @@ public class ProHomeActivity extends HomeActivity {
 			return true;
 		} else {
 			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if (key.equals(getString(R.string.dark_mode))) {
+			toRestart = true;
 		}
 	}
 }
